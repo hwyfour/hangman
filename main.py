af5532439971a5f76d234fba3a104f351c14be14
Dan Harrison <hwyfour@gmail.com>
2016-07-03 20:21:20 -0700

    cleaned up the code formatting

--- main.py
+++ main.py
@@ -1,36 +1,42 @@
 #!/usr/bin/env python
 
-"""main.py - This file contains handlers that are called by taskqueue and/or
-cronjobs."""
+"""
+main.py
+
+This file contains handlers that are called by taskqueue and/or cronjobs.
+"""
+
 import logging
-
 import webapp2
-from google.appengine.api import mail, app_identity
-from api import GuessANumberApi
 
+from api import GuessANumberApi
+from google.appengine.api import mail, app_identity
 from models import User
 
 
 class SendReminderEmail(webapp2.RequestHandler):
+
     def get(self):
         """Send a reminder email to each User with an email about games.
         Called every hour using a cron job"""
+
         app_id = app_identity.get_application_id()
         users = User.query(User.email != None)
+        address = 'noreply@{}.appspotmail.com'.format(app_id)
+
         for user in users:
             subject = 'This is a reminder!'
             body = 'Hello {}, try out Guess A Number!'.format(user.name)
             # This will send test emails, the arguments to send_mail are:
             # from, to, subject, body
-            mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
-                           user.email,
-                           subject,
-                           body)
+            mail.send_mail(address, user.email, subject, body)
 
 
 class UpdateAverageMovesRemaining(webapp2.RequestHandler):
+
     def post(self):
         """Update game listing announcement in memcache."""
+
         GuessANumberApi._cache_average_attempts()
         self.response.set_status(204)
 
--- utils.py
+++ utils.py
@@ -1,14 +1,23 @@
-"""utils.py - File for collecting general utility functions."""
+#!/usr/bin/env python
 
-import logging
-from google.appengine.ext import ndb
+"""
+utils.py
+
+File for collecting general utility functions.
+"""
+
 import endpoints
+import logging
+
+from google.appengine.ext import ndb
+
 
 def get_by_urlsafe(urlsafe, model):
-    """Returns an ndb.Model entity that the urlsafe key points to. Checks
-        that the type of entity returned is of the correct kind. Raises an
-        error if the key String is malformed or the entity is of the incorrect
-        kind
+    """
+    Returns an ndb.Model entity that the urlsafe key points to. Checks that the type of entity
+    returned is of the correct kind. Raises an error if the key String is malformed or the entity
+    is of the incorrect kind.
+
     Args:
         urlsafe: A urlsafe key string
         model: The expected entity kind
@@ -16,9 +25,11 @@ def get_by_urlsafe(urlsafe, model):
         The entity that the urlsafe Key string points to or None if no entity
         exists.
     Raises:
-        ValueError:"""
+        ValueError
+    """
+
     try:
-        key = ndb.Key(urlsafe=urlsafe)
+        key = ndb.Key(urlsafe = urlsafe)
     except TypeError:
         raise endpoints.BadRequestException('Invalid Key')
     except Exception, e:
@@ -28,8 +39,10 @@ def get_by_urlsafe(urlsafe, model):
             raise
 
     entity = key.get()
+
     if not entity:
         return None
     if not isinstance(entity, model):
         raise ValueError('Incorrect Kind')
+
     return entity
